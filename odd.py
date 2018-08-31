ccbff66dac706d9a6e8207da49ed96feaffc9e30
aswinichejarla <42571466+aswinichejarla@users.noreply.github.com>
2018-08-31 11:17:52 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,4 +1,4 @@
 lower,upper = map(int,raw_input().split())
-for n in range(lower,upper+1):
-    if(n%2!=0) and n!=1:
-        print(n),
+for a in range(lower,upper+1):
+    if(a%2!=0) and a!=1:
+        print(a),
