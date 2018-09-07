dbc13819c748fd52dfdf960b26b067a9c0ed61d7
aswinichejarla <42571466+aswinichejarla@users.noreply.github.com>
2018-09-07 11:47:33 +0530

    Update factorial2.py

--- factorial2.py
+++ factorial2.py
@@ -4,5 +4,5 @@ if num == 0:
    print("The factorial of 0 is 1")
 else:
    for i in range(1,num + 1):
-   factorial = factorial*i
-   print(factorial)
+      factorial=factorial*i
+      print(factorial)
