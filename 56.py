c9506dfb04313a848fc28a314fbdee046f9d186c
aswinichejarla <42571466+aswinichejarla@users.noreply.github.com>
2018-09-11 11:41:36 +0530

    Update 56.py

--- 56.py
+++ 56.py
@@ -1,6 +1,6 @@
 n=raw_input()
 a=list(n)
 if("1"in a)or("2"in a)or("3"in a)or("4"in a)or("5"in a)or("6"in a)or("7"in a)or("8"in a)or("9"in a):
-    print "yes"
+    print "Yes"
 else:
     print "No"
