2b740a5f90cd8ab652abe0b7955e082aaa88a46b
aswinichejarla <42571466+aswinichejarla@users.noreply.github.com>
2018-12-05 12:08:50 +0530

    Create a6.py

---
+++ a6.py
@@ -0,0 +1,19 @@
+s1,s2=map(str,raw_input().split())
+MAX_CHARS = 256
+def iso(string1, string2):
+	m = len(string1)
+	n = len(string2)
+	if m != n:
+		return "No"
+	t = [False] * MAX_CHARS
+	map = [-1] * MAX_CHARS
+	for i in range(n):
+		if map[ord(string1[i])] == -1:
+			if t[ord(string2[i])] == True:
+				return "No"
+			t[ord(string2[i])] = True
+			map[ord(string1[i])] = string2[i]
+		elif map[ord(string1[i])] != string2[i]:
+			return "No"
+	return "Yes"
+print (iso(s1,s2))
